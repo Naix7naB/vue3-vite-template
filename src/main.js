3c9428bbfccc95f933944da16b3772e9e5df037a
Naix7naB <duemarsgh@foxmail.com>
2023-12-04 20:52:21 +0800

    :sparkles: feat(projects): 项目引入UnoCss, 新增UnoCss配置和normalize.css

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
+/** 样式引入 */
+import 'uno.css'
+import '@/assets/styles/normalize.css'
+
 import { createApp } from 'vue'
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import VuePlugin from '@vitejs/plugin-vue'
+import UnoCss from 'unocss/vite'
+
+const resolve = dir => path.resolve(__dirname, dir)
 
 export default defineConfig({
     build: {
         outDir: 'dist',
         assetsDir: 'assets'
     },
-    plugins: [VuePlugin()],
+    plugins: [VuePlugin(), UnoCss()],
     resolve: {
         alias: {
-            '@': './src'
+            '@': resolve('src')
         }
     }
 })
