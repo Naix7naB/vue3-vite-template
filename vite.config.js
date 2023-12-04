bf5d17f156e85972ded1885ba68e23e3780a7182
Naix7naB <duemarsgh@foxmail.com>
2023-12-04 16:32:28 +0800

    :sparkles: feat(projects): 项目使用vite+vue3搭建
    项目添加vue和@vitejs/plugin-vue依赖
    配置vite插件支持vue文件
    新增vite执行脚本

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
+import VuePlugin from '@vitejs/plugin-vue'
 
 export default defineConfig({
     build: {
         outDir: 'dist',
         assetsDir: 'assets'
     },
+    plugins: [VuePlugin()],
     resolve: {
         alias: {
             '@': './src'
