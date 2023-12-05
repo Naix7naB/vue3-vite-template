b730c62252ff12d7fa298a7c3b5c847a06045ee2
Naix7naB <duemarsgh@foxmail.com>
2023-12-06 03:44:09 +0800

    :hammer: chore(styles): 新增eslint规则
    新增规则:
    导出模块的顺序与分组
    导出语句需要放在文件最末尾
    如有多个模块需要导出, 不允许单独导出, 需要统一导出
    导入导出具名模块按照英文字母升序排列

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,59 @@
-import antfu from '@antfu/eslint-config'
+import { antfu } from '@antfu/eslint-config'
 
-export default await antfu({
-    vue: true, // 根据依赖自动检测是否开启, 也可以显示手动开启
-    stylistic: {
-        indent: 4, // 缩进4空格
-        semi: false, // 不加分号
-        quotes: 'single' // 使用单引号
+export default await antfu(
+    {
+        vue: true, // 根据依赖自动检测是否开启, 也可以显示手动开启
+        stylistic: {
+            indent: 4, // 缩进4空格
+            semi: false, // 不加分号
+            quotes: 'single' // 使用单引号
+        }
     },
-    rules: {
-        'no-console': ['warn'],
-        'antfu/if-newline': ['off'],
-        'node/prefer-global/process': ['error', 'always'],
-        'style/arrow-parens': ['error', 'as-needed'],
-        'style/brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
-        'style/comma-dangle': ['error', 'never'],
-        'style/eol-last': ['warn', 'always'],
-        'style/function-paren-newline': ['warn', 'consistent'],
-        'style/indent': ['error', 4]
+    {
+        // 全局规则
+        rules: {
+            'no-console': ['warn'],
+            'antfu/if-newline': ['off'],
+            'import/exports-last': ['error'],
+            'import/group-exports': ['error'],
+            'import/order': ['error', {
+                'groups': [
+                    ['builtin', 'external'],
+                    'internal',
+                    ['parent', 'sibling', 'index'],
+                    'object',
+                    'type',
+                    'unknown'
+                ],
+                'newlines-between': 'ignore',
+                'pathGroups': [
+                    {
+                        pattern: '~/**',
+                        group: 'internal',
+                        position: 'before'
+                    },
+                    {
+                        pattern: '@/**',
+                        group: 'internal',
+                        position: 'before'
+                    }
+                ]
+            }],
+            'perfectionist/sort-named-exports': ['error', {
+                type: 'alphabetical',
+                order: 'asc'
+            }],
+            'perfectionist/sort-named-imports': ['error', {
+                type: 'alphabetical',
+                order: 'asc'
+            }],
+            'node/prefer-global/process': ['error', 'always'],
+            'style/arrow-parens': ['error', 'as-needed'],
+            'style/brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
+            'style/comma-dangle': ['error', 'never'],
+            'style/eol-last': ['warn', 'always'],
+            'style/function-paren-newline': ['warn', 'consistent'],
+            'style/indent': ['error', 4]
+        }
     }
-})
+)
